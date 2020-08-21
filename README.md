73d42f1be3ccde008d2239f3c02ac1b3b1edb913
Jaylan <jaylanwienckowski@gmail.com>
2020-08-20 19:19:16 -0500

    added bible verse

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Time
 Just a simple application that provides the time of the day! Hoping to add more soon.
-
+Inspired by Workhard Anywhere.
 # Deployed Link
 https://jaylanw.github.io/time/
--- js/index.js
+++ js/index.js
@@ -11,9 +11,41 @@ function startTime() {
     moment().format("A");
 }
 
+function getDate () {
+document.getElementById('dateTag').innerHTML = 
+  moment().format('dddd') + "," + " " + " " + moment().format('LL');
+}
+
 $(document).ready(function() {
   startTime();
+  getDate();
 });
 
+(function() {
+  $(document ).ready(function() {
+      getVerse();
+  });
 
+  $("button").click(function(){
+    getVerse();
+  });
 
+  var getVerse = function() {
+    $("#spinner").show();
+      $.ajax({
+        url: "https://labs.bible.org/api/?passage=random&type=json&callback=myCallback", 
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(result){
+           $("#newQuote")
+             .html(
+                   result[0].bookname+
+                   ' ' + result[0].chapter +
+                   ':' + result[0].verse );
+            $("#verseText").html (
+                   result[0].text);
+          $("#spinner").hide();
+        }
+      });
+  }
+})();
